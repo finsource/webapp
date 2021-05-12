b7210c98e96403c039b62ad28d31837168c1940a
Aditya Sharma <aditya.sharma9301@gmail.com>
2021-05-12 14:30:25 +0530

    added js

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// this is our js file that is displayed via 'dashboard' section of webapp.
+
+console.log("works");
+
+// async function getNews() {
+//     const datapoint = 'https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=2adaa14ca41640e2820a03ad796ad689';
+//     const response = await fetch(datapoint);
+//     const data = await response.json();
+    
+//     console.log(data);
+// }
+
+// getNews();
+
+// async function getNews() {
+//     await fetch("https://yahoo-finance-low-latency.p.rapidapi.com/v11/finance/quoteSummary/AAPL?modules=defaultKeyStatistics%2CassetProfile", {
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-key": "64e1b6ec71msh79f17be5fb5db5dp1ecd69jsnb974c96d020c",
+//             "x-rapidapi-host": "yahoo-finance-low-latency.p.rapidapi.com"
+//         }
+//     })
+//     .then(response => {
+//         console.log(response);
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
+// }
+
+// getNews();
