a170e3bfa8646f6bd8193f1d3ffd6ac88b8a373b
Aditya Sharma <aditya.sharma9301@gmail.com>
2021-05-14 22:28:16 +0530

    added db,php

--- js/database.js
+++ js/database.js
@@ -1,14 +1,32 @@
-const app = express();
+// const app = express();
 
-const dataStore = require('nedb');
-const database = new dataStore('../data/database.db');
+// const dataStore = require('nedb');
+// const database = new dataStore('../data/database.db');
 
-database.loadDatabase();
+// database.loadDatabase();
 
-window.document.getElementById('submit').addEventListener('click' , () => {
+// window.document.getElementById('submit').addEventListener('click' , () => {
+//     const name = document.getElementById('name_input').value;
+//     const email = document.getElementById('email-input').value;
+//     const phone = document.getElementById('phone-input').value;
+
+//     database.insert(`{name : ${name} , email : ${email} , phone : ${phone}}`);
+// });
+
+function send_data() {
     const name = document.getElementById('name_input').value;
     const email = document.getElementById('email-input').value;
     const phone = document.getElementById('phone-input').value;
 
-    database.insert(`{name : ${name} , email : ${email} , phone : ${phone}}`);
-});
+    var httpr = new XMLHttpRequest();
+    httpr.open("POST" , "../php/main.php" , true);
+    httpr.setRequestHeader('content-type' , 'application');
+
+    httpr.onreadystatechange = function() {
+        if (httpr.readyState == 4 && httpr.status == 200) {
+            alert('data sent to database!');
+        }
+    };
+
+    httpr.send('name =' + name + "email =" + email + "phone = " + phone);
+}
