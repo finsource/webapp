cddc2ddbfa3da15e0072cfe96b84757d57762ba4
Aditya Sharma <aditya.sharma9301@gmail.com>
2021-05-19 16:50:25 +0530

    added stocks section

--- js/stocks.js
+++ js/stocks.js
@@ -1,1 +1,50 @@
-console.log("hello from stcoks.js");
+async function getStocks() {
+
+    const respone = await fetch(
+        "https://latest-stock-price.p.rapidapi.com/price?Indices=NIFTY%2050",
+        {
+            method: "GET",
+            headers: {
+                "x-rapidapi-key": "64e1b6ec71msh79f17be5fb5db5dp1ecd69jsnb974c96d020c",
+                "x-rapidapi-host": "latest-stock-price.p.rapidapi.com",
+            },
+        }
+    );
+
+    const data = await respone.json();
+    //   console.log(data);
+
+    //   const stocks = ['stoc']
+
+    for (let i = 1; i <= 8; i++) {
+
+        let num = `stock${i}-card`;
+        let card = document.getElementById(num);
+        // console.log(card);
+
+        let stock_name = card.getElementsByClassName("stock-name-card");
+        stock_name = stock_name[0];
+
+        stock_name.innerText = data[i].symbol;
+
+        let stock_code = card.getElementsByClassName("stock-short");
+        stock_code = stock_code[0];
+        stock_code.innerText = `#${i}`;
+
+        let stock_price = card.getElementsByClassName("card-price");
+        stock_price = stock_price[0];
+        stock_price.innerText = `₹${data[i].lastPrice}`;
+
+        let stock_change = card.getElementsByClassName("card-price-change");
+        stock_change = stock_change[0];
+        stock_change.innerText = `${data[i].pChange}%`;
+
+        if (data[i].pChange > 0) {
+            stock_change.style.color = "#33cc33";
+        } else {
+            stock_change.style.color = "red";
+        }
+    }
+}
+
+window.onload = getStocks();
